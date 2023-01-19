42937b6d970a827220dc966278745019b8f85ac9
RamadhanS123 <111478906+RamadhanS123@users.noreply.github.com>
2023-01-20 06:12:35 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Website</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+     <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="navbar-brand" href="index.php">Home</a>
+    </li>
+  </ul>
+</nav>
+<div class="container">
+  <h2>Profil</h2>
+  <p></p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Nama</th>
+        <th>Jenis Kelamin</th>
+		<th>Agama</th>
+		<th>Status</th>
+		<th>Alamat</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Ramadhan Saputra Alpani</td>
+        <td>Laki-Laki</td>
+        <td>Islam</td>
+		<td>Belum Menikah</td>
+		<td>Jl.Merawan 20 sawah lebar</td>
+		
+      </tr>
+    </tbody>
+  </table>
+</div>
+  </div>
+</div>
+<div class="jumbotron text-center" style="margin-bottom:0">
+<ul class="pagination">
+  <li class="page-item"><a class="page-link" href="page1.php?page=page1.php">Previous</a></li>
+  <li class="page-item"><a class="page-link" href="index.php">1</a></li>
+  <li class="page-item"><a class="page-link" href="page1.php?page=page1.php">2</a></li>
+  <li class="page-item"><a class="page-link" href="page2.php?page=page2.php">3</a></li>
+  <li class="page-item"><a class="page-link" href="index.php">Next</a></li>
+</ul>
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>2023</p>
+</div>
+</body>
+</html>
